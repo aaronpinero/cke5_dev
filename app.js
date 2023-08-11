61238ac12943b0ac9c0e33a87295adb8b9df74c7
Aaron Pinero <ap2963@cumc.columbia.edu>
2023-08-11 12:18:27 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// app.js
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+
+import SimpleBox from './simplebox/simplebox';
+
+import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
+
+ClassicEditor
+  .create( document.querySelector( '#editor' ), {
+    plugins: [ Alignment, Essentials, Paragraph, Bold, Italic, Heading, Indent, Link, List, Table, TableToolbar, SimpleBox ],
+    toolbar: [ 'alignment', 'heading', '|', 'bold', 'italic', 'link', '|', 'bulletedList', 'numberedList', 'outdent', 'indent', '|', 'insertTable' ],
+    table: {
+      contentToolbar: [
+        'tableColumn',
+        'tableRow',
+        'mergeTableCells'
+      ]
+    }
+  } )
+  .then( editor => {
+    console.log( 'Editor was initialized', editor );
+    CKEditorInspector.attach( editor );
+  } )
+  .catch( error => {
+    console.error( error.stack );
+  } );
---
+++ detail/src/detailui.js
@@ -0,0 +1,35 @@
+/**
+ * @file registers the detail toolbar button and binds functionality to it.
+ */
+
+import { Plugin } from '@ckeditor/ckeditor5-core';
+import { ButtonView } from '@ckeditor/ckeditor5-ui';
+
+export default class DetailUI extends Plugin {
+  init() {
+    const editor = this.editor;
+
+    // This will register the detail toolbar button.
+    editor.ui.componentFactory.add('detail', (locale) => {
+      const command = editor.commands.get('insertDetail');
+      const buttonView = new ButtonView(locale);
+
+      // Create the toolbar button.
+      buttonView.set({
+        label: Drupal.t('Add details'),
+        withText: true,
+        tooltip: true,
+      });
+
+      // Bind the state of the button to the command.
+      buttonView.bind('isOn', 'isEnabled').to(command, 'value', 'isEnabled');
+
+      // Execute the command when the button is clicked (executed).
+      this.listenTo(buttonView, 'execute', () =>
+        editor.execute('insertDetail'),
+      );
+
+      return buttonView;
+    });
+  }
+}
